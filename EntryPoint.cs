478800b5c9b54265c4f050888d89dadde1c487d5
Somad <iretibe@users.noreply.github.com>
2022-03-18 15:23:31 +0000

    Committed Selenium repository

---
+++ EntryPoint.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace SeleniumApp
+{
+    internal class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("http://testing.todorvachev.com");
+
+            Thread.Sleep(3000);
+            driver.Quit();
+        }
+    }
+}
